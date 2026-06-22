e5d13b3deb25d0b785be27145cf5e76badf65c89
bfjy2024 <slwu19@163.com>
2026-06-22 22:58:58 +0800

    Update __init__.py

--- plugins.v2/playletlottery/__init__.py
+++ plugins.v2/playletlottery/__init__.py
@@ -27,7 +27,7 @@ class PlayletLottery(_PluginBase):
     plugin_name = "PlayLet自动抽奖助手"
     plugin_desc = "按每日目标次数自动拆解并执行 PlayLet 抽奖。"
     plugin_icon = "Moviepilot_A.png"
-    plugin_version = "1.0.3"
+    plugin_version = "1.1.0"
     plugin_author = "jiangbkvir,bfjy"
     author_url = "https://github.com/jiangbkvir/MoviePilot-Plugins"
     plugin_config_prefix = "playletlottery_"
